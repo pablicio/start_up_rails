c5eee6af7e874360cbc55d652cdf86fc547de2d7
Altran <thiago.nogueira.altran@caixaseguradora.com.br>
2020-12-08 21:37:30 -0300

    Refatoração de ambiente base

--- Vagrantfile
+++ Vagrantfile
@@ -1,16 +1,27 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
+Vagrant.configure('2') do |config|
+  config.vm.box      = 'ubuntu/groovy64' # 20.10
+  config.vm.hostname = 'rails-dev-box'
 
-Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/bionic64"
-  config.vm.network "private_network", ip: "192.168.55.200"
-  config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.provision :shell, path: "build/provision.sh"
-  config.vm.provision :shell, path: "build/rabbitmq.sh"
-  config.vm.provision :shell, path: "build/user.sh"
+  config.vm.network :forwarded_port, guest: 5432, host: 5432 # postgresql
+  config.vm.network :forwarded_port, guest: 6379, host: 6379 # redis
+
+  config.vm.network :forwarded_port, guest: 5672, host: 5672 # rails app
+  config.vm.network :forwarded_port, guest: 15672, host: 15672 # rabbitmq
+  config.vm.network :forwarded_port, guest: 25672, host: 25672 # rabbitmq
+
+  config.vm.network :forwarded_port, guest: 3000, host: 3000 # rails app
+  config.vm.network :forwarded_port, guest: 1443, host: 1443 # rails app
+
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  config.ssh.forward_agent = true
 
 
-  config.ssh.extra_args = ["-t", "cd /vagrant; bash --login"]
+  config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
+
+  config.ssh.extra_args = ["-t", "cd /vagrant; bash --login;"]
 
   config.vm.provider "virtualbox" do |v|
     v.memory = 2048
---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+# The output of all these installation steps is noisy. With this utility
+# the progress report is nice and concise.
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
+
+echo adding swap file
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
+# Prevents "Warning: apt-key output should not be parsed (stdout is not a terminal)".
+export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo -E apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+echo updating package information
+apt-get -y update >/dev/null 2>&1
+
+# echo installing Ruby
+echo 'INSTALAÇÃO RUBY'
+install Ruby ruby-full
+install 'development tools' build-essential autoconf libtool
+
+# echo installing current RubyGems
+echo 'INSTALAÇÃO CURRENT GEMS'
+
+gem update --system -N >/dev/null 2>&1
+
+echo installing Bundler
+gem install bundler -N >/dev/null 2>&1
+
+install SQLite sqlite3 libsqlite3-dev
+
+install RabbitMQ rabbitmq-server
+
+#instalar POSTGRESQL
+echo 'INSTALAÇÃO POSTGRESQL'
+
+install PostgreSQL postgresql postgresql-contrib libpq-dev
+sudo -u postgres createuser --superuser vagrant
+sudo -u postgres psql -c "ALTER USER vagrant PASSWORD 'vagrant';"
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres'; "
+
+sudo -u postgres createdb -O vagrant -E UTF8 -T template0 activerecord_unittest
+sudo -u postgres createdb -O vagrant -E UTF8 -T template0 activerecord_unittest2
+
+
+#instalar RVM
+
+
+
+echo 'A festa começa aqui, vamo balançar!!!'
