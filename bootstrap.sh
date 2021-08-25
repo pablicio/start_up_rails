9b479b0ad6ac9863c70b4dc9879df5ca1164ce05
unknown <pabliciotjg@gmail.com>
2021-08-25 14:02:13 -0300

    Adição de atualização de vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,11 @@ sudo -u postgres createdb -O vagrant -E UTF8 -T template0 activerecord_unittest2
 
 
 #instalar RVM
+echo 'INSTALAÇÃO RVM'
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s $1
+sudo chown -R vagrant:rvm /usr/share/rvm/*
 
 
 echo 'A festa começa aqui, vamo balançar!!!'
