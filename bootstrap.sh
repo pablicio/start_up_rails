5c9a5a2c068d90973115ae8adff768cb49787d84
Pablicio <pabliciotjg@gmail.com>
2023-01-31 14:00:20 -0300

    Excluindo comentarios

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,8 +24,6 @@ sudo apt update
 sudo apt install gnupg2
 gpg2 --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io -o rvm.sh
-# nano rvm.sh
-# cat rvm.sh | bash -s stable --rails
 source ~/.rvm/scripts/rvm
 
 #
